eb8b6ec8e0b3318039c58ea7513ec385c021e330
Rufat0 <47609502+Rufat0@users.noreply.github.com>
2019-03-12 10:34:57 -0700

    Add files via upload

---
+++ problem 1.py
@@ -0,0 +1,21 @@
+a = int(input("birinci reqem"))
+
+b = int(input("ikinci reqem"))
+
+c = int(input("ucuncu reqem"))
+
+
+if a == b== c:
+    print("ucude beraberdir")
+
+elif a == b :
+    print("birinci ve ikinci beraberdir ")
+
+elif b == c:
+    print("ikinci ile ucuncu beraberdir")
+
+elif a == c:
+    print("birinci ile ucuncu beraberdir ")
+
+else:
+    print("hec bir reqem bir birine beraber deil")
